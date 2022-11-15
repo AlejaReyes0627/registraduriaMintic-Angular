45b24dfc642e5cf9bd60b86842123c420a86b7d8
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-11-14 23:59:21 -0500

    Controlador

--- src/app/pages/seguridad/login/login.component.ts
+++ src/app/pages/seguridad/login/login.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import Swal from "sweetalert2";
+import { Usuario } from "../../../modelos/usuario.model";
+import { SeguridadService } from "../../../servicios/seguridad.service";
 
 @Component({
-  selector: 'ngx-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "ngx-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
+  correo: string = "";
+  contrasena: string = "";
+  constructor(
+    private miServicioSeguridad: SeguridadService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login(): void {
+    console.log("aqui" + this.correo + " contraseña " + this.contrasena);
+    let elUsuario: Usuario = {
+      correo: this.correo,
+      contrasena: this.contrasena,
+    };
+    this.miServicioSeguridad.login(elUsuario).subscribe(
+      (data) => {
+        this.router.navigate(["pages/dashboard"]);
+        this.miServicioSeguridad.guardarDatosSesion(data);
+      },
+      (error) => {
+        Swal.fire({
+          title: "Error Login",
+          text: error["error"]["message"],
+          icon: "error",
+          timer: 5000,
+        });
+      }
+    );
   }
-
 }
